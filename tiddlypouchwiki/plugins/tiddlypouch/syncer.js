e55c506227b3cd3e3590592f3e93767fb14882f5
danielo515 <rdanielo@gmail.com>
2015-05-31 11:55:05 +0200

    Mayor re-design of sync module startSync is now a method of TP global object Created newonlineDB as a method of global TP object Use utils getConfig for config read

--- tiddlypouchwiki/plugins/tiddlypouch/syncer.js
+++ tiddlypouchwiki/plugins/tiddlypouch/syncer.js
@@ -27,7 +27,8 @@ exports.startup = function(callback) {
   this.logger = new $tw.utils.Logger("PouchSync");
   var self=this;
   this.logger.log('Trying to sync...');
-  function PouchLog(log,info){
+ 
+ function PouchLog(log,info){
     /*Appends info to the specified log tiddler*/
     var oldinfo = $tw.wiki.getTiddlerText(log) + '\n';
     var newinfo = typeof info === 'object' ? JSON.stringify(info,null,$tw.config.preferences.jsonSpaces) : info;
@@ -35,36 +36,21 @@ exports.startup = function(callback) {
     $tw.wiki.addTiddler(new $tw.Tiddler(logTiddler));
     self.logger.log(info);
   }
-
-    var URL = $tw.wiki.getTiddlerText(CONFIG_PREFIX + 'URL');
-    var Databasename = $tw.wiki.getTiddlerText(CONFIG_PREFIX + 'RemoteDatabaseName');
-    var sincStatusFlag = $tw.wiki.getTiddler(SYNC_ICON);
-    /*If there is no URL set, then no sync*/
-    if(!URL){
-        this.logger.log("Entering offline mode");
-        /* We don't want sync status icon on sidebar*/
-        $tw.wiki.addTiddler(new $tw.Tiddler(sincStatusFlag,{tags:[]})); //so remove tags
-        callback();
-        return
-    }
-        /*Otherwise, add to sidebar with the tag (it could be removed) */
-        $tw.wiki.addTiddler(new $tw.Tiddler(sincStatusFlag,{tags:['$:/tags/ControlPanel']})); //so remove tags
-        
-    URL = URL.substr(-1) === '/' ? URL.trim() : URL.trim() + '/'; //Make sure it ends with slash
-    Databasename = Databasename.trim();
-    var sync = $tw.TiddlyPouch.PouchDB.sync($tw.TiddlyPouch.database, URL+Databasename, {
+  
+  function startSync(remoteDB){
+      /*To-do: Should check if local db exists and if there is another sync going*/
+      var sync = $tw.TiddlyPouch.PouchDB.sync($tw.TiddlyPouch.database, remoteDB, {
         live: true,
         retry: true
     }).on('change', function (info) {
-        self.logger.log(info)
         PouchLog(SYNC_LOG,info);
     }).on('paused', function () {
         $tw.wiki.setText(SYNC_STATE,'text',undefined,'paused')
         PouchLog(SYNC_LOG,"===SYNC PAUSED===");
     }).on('active', function () {
-        self.logger.log('replicate resumed (e.g. user went back online)');
         $tw.wiki.setText(SYNC_STATE,'text',undefined,'syncing');
-        PouchLog(SYNC_LOG,"===SYNC ACTIVE===")
+        PouchLog(SYNC_LOG,"===SYNC ACTIVE===");
+        PouchLog(SYNC_LOG,"replicate resumed (e.g. user went back online)");
     }).on('denied', function (info) {
         // a document failed to replicate
         PouchLog(SYNC_ERRORS,info);
@@ -75,6 +61,36 @@ exports.startup = function(callback) {
         PouchLog(SYNC_ERRORS,err);
     });
     $tw.TiddlyPouch.syncHandler=sync;
+  }
+  
+  function newOnlineDB(){
+      var utils = $tw.TiddlyPouch.utils;
+      var URL = utils.getConfig('URL');
+      var Databasename = utils.getConfig('RemoteDatabaseName');
+      var sincStatusFlag = $tw.wiki.getTiddler(SYNC_ICON);
+      /*If there is no URL set, then no sync*/
+      if(!URL){
+          PouchLog(SYNC_LOG,"Entering offline mode");
+          /* We don't want sync status icon on sidebar*/
+          $tw.wiki.addTiddler(new $tw.Tiddler(sincStatusFlag,{tags:[]})); //so remove tags
+          return
+       }
+        /*Otherwise, add to sidebar with the tag (it could be removed) */
+        $tw.wiki.addTiddler(new $tw.Tiddler(sincStatusFlag,{tags:['$:/tags/PageControls']}));
+        
+       URL = URL.substr(-1) === '/' ? URL : URL + '/'; //Make sure it ends with slash
+       
+       return $tw.TiddlyPouch.PouchDB(URL + Databasename);
+  }
+  
+    $tw.TiddlyPouch.startSync = startSync;
+    $tw.TiddlyPouch.newOnlineDB = newOnlineDB;
+    var onlineDB = newOnlineDB();
+    if(!onlineDB){
+        callback('There is no online DB set');
+        return
+    }
+    startSync(onlineDB);
     callback();
 };
 
