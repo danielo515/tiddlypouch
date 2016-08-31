df696f1c89d6d60659081b930834c4b746b68b6d
Danielo Rodriguez <rdanielo@gmail.com>
2016-08-31 02:03:09 +0200

    Major minor and patch management

--- gulpfile.js
+++ gulpfile.js
@@ -137,15 +137,18 @@ gulp.task("perform cleanup", function() {
  */
 gulp.task("bump_version", function(cb) {
 
-  var v = new SemVer(pluginInfo.version);
-  var build = (isIncrBuild ? "+" + (parseInt(v.build[0] || 0) + 1) : "");
-  var mode = (argv.mode && argv.mode !== "master" ? "-" + argv.mode : "");
-  pluginInfo.version = v.major + "." + v.minor + "." + v.patch + mode + build;
-  pluginInfo.released = new Date().toUTCString();
+    var v = new SemVer(pluginInfo.version);
+    var build = (isIncrBuild ? "+" + (parseInt(v.build[0] || 0) + 1) : "");
+    var mode = (argv.mode && argv.mode !== "master" ? "-" + argv.mode : "");
+    argv.major && v.major++ && (v.minor = 0);
+    argv.minor && v.minor++ && (v.patch = 0);
+    argv.patch && v.patch++;
+    pluginInfo.version = v.major + "." + v.minor + "." + v.patch + mode + build;
+    pluginInfo.released = new Date().toUTCString();
 
-  fs.writeFileSync(pluginInfoPath, JSON.stringify(pluginInfo, null, 2));
+    fs.writeFileSync(pluginInfoPath, JSON.stringify(pluginInfo, null, 4));
 
-  cb();
+    cb();
 
 });
 
