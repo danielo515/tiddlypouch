0670df620184b58dcb669cfc567f8186d33821df
Danielo Rodriguez Rivero <rdanielo@gmail.com>
2017-06-11 10:13:43 +0200

    - Added controls to the revisions tab to restore a previous revision FIX #23

--- src/plugins/danielo515/tiddlypouch/Logger.js
+++ src/plugins/danielo515/tiddlypouch/Logger.js
@@ -12,9 +12,9 @@ A basic logging implementation
 
 /*jslint node: true, browser: true */
 /*global $tw: false */
-"use strict";
+  'use strict';
 
-var Super = require('$:/core/modules/utils/logger.js').Logger;
+  var Super = require('$:/core/modules/utils/logger.js').Logger;
 
 /**
 * Make a new logger
@@ -24,42 +24,42 @@ var Super = require('$:/core/modules/utils/logger.js').Logger;
 * @param {boolean} verbose - if verbose debug level is active (used on trace method)
 *
 */
-function Logger(componentName, debug , verbose ) {
+  function Logger(componentName, debug , verbose ) {
     Super.call(this,componentName);
 
-    if( typeof debug === "object" ){
-        verbose = debug.verbose;
-        debug = debug.debug
+    if ( typeof debug === 'object' ){
+      verbose = debug.verbose;
+      debug = debug.debug;
     }
     this.isDebug = debug;
     this.isVerbose = verbose;
-}
+  }
 
-Logger.prototype = Object.create(Super.prototype);
-Logger.prototype.constructor = Super;
+  Logger.prototype = Object.create(Super.prototype);
+  Logger.prototype.constructor = Super;
 
 /**
  * Log only if there is debug enabled
  * @returns {undefined} returns nothing
  */
-Logger.prototype.debug = function(){
-    if(!this.isDebug){
-        return 
+  Logger.prototype.debug = function(){
+    if (!this.isDebug){
+      return;
     }
-    this.log.apply(this, Array.prototype.slice.call(arguments,0))
-}
+    this.log.apply(this, Array.prototype.slice.call(arguments,0));
+  };
 
 /**
  * Log only if debug is verbose
  * @returns {undefined} returns nothing
  */
-Logger.prototype.trace = function(){
-    if(!this.isVerbose){
-        return 
+  Logger.prototype.trace = function(){
+    if (!this.isVerbose){
+      return;
     }
-    this.debug.apply(this, Array.prototype.slice.call(arguments,0))
-}
+    this.debug.apply(this, Array.prototype.slice.call(arguments,0));
+  };
 
-exports.Logger = Logger;
+  exports.Logger = Logger;
 
 }());
