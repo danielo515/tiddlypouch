52d509708a91d6de1c49672e0dce6b6488f073e5
Daniel Rodríguez Rivero <rdanielo@gmail.com>
2020-04-25 19:36:52 +0200

    Allow to load fat tiddlers (#100)
    * chore: remove old assets
    
    * feat: tiddlers can now be loaded as fat if desired
    
    * chore: tw rollback
    
    * chore: for god's shake, eslint!
    
    * chore: gulp now builds
    
    * chore: simpler wiki folder name
    
    * chore: changelog
    
    * chore: proper build

--- src/plugins/danielo515/tiddlypouch/ui-config.js
+++ src/plugins/danielo515/tiddlypouch/ui-config.js
@@ -9,58 +9,66 @@ Links the user interface with the configuration methods
 
 \*/
 
-'use strict';
+"use strict";
 /** @module */
 
-
+//@ts-check
 
 /*jslint node: true, browser: true */
 /*global $tw: false */
 
-var DEBUG_CONFIG = '$:/plugins/danielo515/tiddlypouch/config/Debug';
-var DEBUG_ACTIVE = DEBUG_CONFIG + '/Active';
-var DEBUG_VERBOSE = DEBUG_CONFIG + '/Verbose';
-var SELECTED_DATABASE = '$:/plugins/danielo515/tiddlypouch/config/selected_database';
-const { 
-  CONFIG_SAVED, SYNC_ICON, DATABASE_NAMES 
-  } = require('$:/plugins/danielo515/tiddlypouch/constants.js');
+/**
+ * @typedef {import('./startup/startup-config').tpouchConfig} tpouchConfig
+ */
 
-var Utils = require('$:/plugins/danielo515/tiddlypouch/utils');
+var SELECTED_DATABASE =
+    "$:/plugins/danielo515/tiddlypouch/config/selected_database";
+const {
+    CONFIG_SAVED,
+    SYNC_ICON,
+    DATABASE_NAMES,
+    DEBUG_CONFIG,
+} = require("$:/plugins/danielo515/tiddlypouch/constants.js");
+
+var Utils = require("$:/plugins/danielo515/tiddlypouch/utils");
 
 $TPouch.ui = $TPouch.ui || {};
 
-$TPouch.ui.refresh =
-  exports.refreshUI =
-  function refreshUI(config) {
+$TPouch.ui.refresh = exports.refreshUI = function refreshUI(config) {
     updateDebugUI(config);
     refreshSelectedDbUi(config.databases[config.selectedDbId]);
     setSyncFlag();
     refreshDatabaseNamesUI();
     setLoginMessage();
     setSiteSubtitleToDatabaseName();
-  };
+};
 
 exports.handlers = {};
 
 function setSyncFlag(mode) {
-  var sincStatusFlag = $tw.wiki.getTiddler(SYNC_ICON);
-  /* Because I'm unsure about how to decide if we are in offline mode
+    var sincStatusFlag = $tw.wiki.getTiddler(SYNC_ICON);
+    /* Because I'm unsure about how to decide if we are in offline mode
       i can take both, a message param or just a plain function execution.
       take a look at startup-syncer for the emit of the events
   */
-  if (mode === 'offline' || !$TPouch.config.currentDB.getUrl()) {
-    /* We don't want sync status icon on sidebar*/
-    return $tw.wiki.addTiddler(new $tw.Tiddler(sincStatusFlag, { tags: [] })); //Remove tags
-  }
-  /*Otherwise, add to sidebar with the tag (it could be removed) */
-  $tw.wiki.addTiddler(new $tw.Tiddler(sincStatusFlag, { tags: ['$:/tags/PageControls'] }));
+    if (mode === "offline" || !$TPouch.config.currentDB.getUrl()) {
+        /* We don't want sync status icon on sidebar*/
+        return $tw.wiki.addTiddler(
+            new $tw.Tiddler(sincStatusFlag, { tags: [] })
+        ); //Remove tags
+    }
+    /*Otherwise, add to sidebar with the tag (it could be removed) */
+    $tw.wiki.addTiddler(
+        new $tw.Tiddler(sincStatusFlag, { tags: ["$:/tags/PageControls"] })
+    );
 }
 
 exports.setSyncFlag = setSyncFlag;
 
 function setSiteSubtitleToDatabaseName() {
-  var text = '<<tiddlypouch-tab ' + $TPouch.config.currentDB.name + ' Database >>';
-  $tw.wiki.addTiddler({ title: '$:/SiteSubtitle', text: text });
+    var text =
+        "<<tiddlypouch-tab " + $TPouch.config.currentDB.name + " Database >>";
+    $tw.wiki.addTiddler({ title: "$:/SiteSubtitle", text: text });
 }
 
 /**
@@ -68,37 +76,75 @@ function setSiteSubtitleToDatabaseName() {
  * to remote URL of the current database.
  */
 function setLoginMessage() {
-  var loginDestination = $TPouch.config.currentDB.getUrl();
-  var databaseName = $TPouch.config.currentDB.getRemoteName();
-  var message = 'Login to remote database <b>' + databaseName + '</b> at: ' + loginDestination;
-  $tw.wiki.addTiddler({
-    title: '$:/language/LoginToTiddlySpace',
-    text: message
-  });
-
+    var loginDestination = $TPouch.config.currentDB.getUrl();
+    var databaseName = $TPouch.config.currentDB.getRemoteName();
+    var message =
+        "Login to remote database <b>" +
+        databaseName +
+        "</b> at: " +
+        loginDestination;
+    $tw.wiki.addTiddler({
+        title: "$:/language/LoginToTiddlySpace",
+        text: message,
+    });
 }
 
 function refreshDatabaseNamesUI() {
-  var namesList = $TPouch.config.getAllDBNames();
-  $tw.wiki.addTiddler({ title: DATABASE_NAMES, list: namesList, text: '{{!!list}}' });
+    var namesList = $TPouch.config.getAllDBNames();
+    $tw.wiki.addTiddler({
+        title: DATABASE_NAMES,
+        list: namesList,
+        text: "{{!!list}}",
+    });
+}
+
+function tryParse(value) {
+    try {
+        return JSON.parse(value);
+    } catch (err) {
+        return value;
+    }
+}
+/**
+ * Returns the fields of a tiddler as a POJO
+ * parsing values as needed (booleans, numbers, arrays...)
+ * @param {string} title the title of the tiddler
+ */
+function getTiddlerFields(title) {
+    const tiddler = $tw.wiki.getTiddler(title) || { fields: {} };
+    const result = {};
+    $tw.utils.each(tiddler.fields, (val, key) => {
+        result[key] = tryParse(val);
+    });
+    return result;
 }
 
 exports.handlers.updateDebug = function (event) {
-  var Active = $tw.wiki.getTiddlerText(DEBUG_ACTIVE) === 'yes';
-  var Verbose = $tw.wiki.getTiddlerText(DEBUG_VERBOSE) === 'yes';
-  var savedConfig = $TPouch.config.readConfigTiddler();
-  savedConfig.debug = { active: Active, verbose: Verbose };
-  $TPouch.config.update(savedConfig);
+    var rawConfig = getTiddlerFields(DEBUG_CONFIG);
+    //@type { tpouchConfig }
+    var savedConfig = $TPouch.config.readConfigTiddler();
+    savedConfig.debug = {
+        active: rawConfig.debug,
+        verbose: rawConfig.verbose,
+    };
+    savedConfig.useFatTiddlers = rawConfig.useFatTiddlers;
+    $TPouch.config.update(savedConfig);
 };
 
 /**
  *  Updates the debug ui to reflect the configuration
  *
- * @param {Object} config
+ * @param {tpouchConfig} config
  */
 function updateDebugUI(config) {
-  $tw.wiki.addTiddler(new $tw.Tiddler({ title: DEBUG_ACTIVE, text: Utils.boolToHuman(config.debug.active) }));
-  $tw.wiki.addTiddler(new $tw.Tiddler({ title: DEBUG_VERBOSE, text: Utils.boolToHuman(config.debug.verbose) }));
+    $tw.wiki.addTiddler(
+        new $tw.Tiddler({
+            title: DEBUG_CONFIG,
+            debug: JSON.stringify( config.debug.active ),
+            verbose: JSON.stringify( config.debug.verbose ),
+            useFatTiddlers: JSON.stringify( config.useFatTiddlers ),
+        })
+    );
 }
 
 /**
@@ -108,19 +154,24 @@ function updateDebugUI(config) {
  * the user can select a DB different than the current one and save that config.
  */
 exports.handlers.updateDbConfig = function (event) {
-  const uiConfig = $tw.wiki.getTiddlerData(SELECTED_DATABASE);
-  const updateDescription = {
-    // Instead of updating fields separately, we build an object describing just the new sections to add
-    // and the update method will take care of updating the corresponding parts
-    selectedDbId: uiConfig.name,
-    databases: {
-      [uiConfig.name]: Utils.plainToNestedObject(uiConfig)
-    }
-  };
+    const uiConfig = $tw.wiki.getTiddlerData(SELECTED_DATABASE);
+    const debugConfig = getTiddlerFields(DEBUG_CONFIG); //TODO: unify both configs and save them at once
+    //@type { tpouchConfig }
+    const updateDescription = {
+        // Instead of updating fields separately, we build an object describing just the new sections to add
+        // and the update method will take care of updating the corresponding parts
+        selectedDbId: uiConfig.name,
+        useFatTiddlers: debugConfig.useFatTiddlers,
+        databases: {
+            [uiConfig.name]: Utils.plainToNestedObject(uiConfig),
+        },
+    };
 
-  $TPouch.config
-    .update(updateDescription)
-    .then(() => $tw.rootWidget.dispatchEvent({ type: CONFIG_SAVED, param: true })); // when saved from UI, ask for a reboot
+    $TPouch.config
+        .update(updateDescription)
+        .then(() =>
+            $tw.rootWidget.dispatchEvent({ type: CONFIG_SAVED, param: true })
+        ); // when saved from UI, ask for a reboot
 };
 
 /**
@@ -128,9 +179,9 @@ exports.handlers.updateDbConfig = function (event) {
  * It loads it's configuration and refreshes the UI with it.
  */
 exports.handlers.databaseHasBeenSelected = function (event) {
-  var dbName = event.param;
-  var dbConfig = $TPouch.config.getDatabaseConfig(dbName);
-  refreshSelectedDbUi(dbConfig);
+    var dbName = event.param;
+    var dbConfig = $TPouch.config.getDatabaseConfig(dbName);
+    refreshSelectedDbUi(dbConfig);
 };
 
 /**
@@ -139,11 +190,13 @@ exports.handlers.databaseHasBeenSelected = function (event) {
  * @param {remoteConfig} dbConfig
  */
 function refreshSelectedDbUi(dbConfig) {
-  //var dbInfo = config.databases[config.selectedDbId];
-  var uiConfig = Utils.flattenObject(dbConfig);
-  $tw.wiki.addTiddler(new $tw.Tiddler({
-    title: SELECTED_DATABASE,
-    type: 'application/json',
-    text: JSON.stringify(uiConfig)
-  }));
+    //var dbInfo = config.databases[config.selectedDbId];
+    var uiConfig = Utils.flattenObject(dbConfig);
+    $tw.wiki.addTiddler(
+        new $tw.Tiddler({
+            title: SELECTED_DATABASE,
+            type: "application/json",
+            text: JSON.stringify(uiConfig),
+        })
+    );
 }
